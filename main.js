474ea2fa42ba6ce95269c3e5fe7458f8ddac5cee
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-03-15 15:57:48 +0300

    done

---
+++ main.js
@@ -0,0 +1,17 @@
+alert("Sizda hozir random sonlar chiqishni boshlaydi!!!!");
+let first = (Math.round(Math.random() *100));
+alert(`${"Random orqali chiqgan son:"}${first}`)
+alert(`${"Biz random sonni 2 ga kopaytirmoqchi va bo'lmoqchimiz!"}`)
+
+alert(`${"Random orqali chiqgan son:"}${first}${"edi"} \n ${"boiz uni 2ga kopaytirib"} ${first*2} ${"sonini chiqardik,"} \n  ${"va shu sonni 2 ga bolib:"} ${first/2} ${"sonini chiqardik,"} \n ${"va shu sonni 3 ga bolibgandagi qoldiq:"} ${first%3} ${"sonini chiqardiki"}`)
+
+
+
+
+
+alert("Sizda hozir random sonlar chiqishni boshlaydi!!!!");
+let a = (Math.round(Math.random() *100));
+alert(`${"Random orqali chiqgan son:"}${a}`)
+alert(`${"Biz random sonni 2 ga kopaytirmoqchi va bo'lmoqchimiz!"}`)
+
+alert(`${"Random orqali chiqgan son:"}${a}${"edi"} \n ${"boiz uni 2ga kopaytirib"} ${a*2} ${"sonini chiqardik,"} \n  ${"va shu sonni 2 ga bolib:"} ${a/2} ${"sonini chiqardik,"} \n ${"va shu sonni 3 ga bolibgandagi qoldiq:"} ${a%3} ${"sonini chiqardiki"}`)
